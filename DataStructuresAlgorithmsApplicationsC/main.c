f2ad8628047cd6c34c0afe2f649e56a9019b49ac
shuimuyangsha <1023178387@qq.com>
2020-12-10 19:52:40 +0800

    添加项目文件。

---
+++ DataStructuresAlgorithmsApplicationsC/main.c
@@ -0,0 +1,36 @@
+//#include "aatree.h"
+//#include "queue.h"
+#include "stdio.h"
+#include "fatal.h"
+
+/*3.4 队列*/
+//Queue q1 = NULL;
+//int main(void)
+//{
+//	int i;
+//	
+//	Queue q1 = CreateQueue(100);
+//	for (i = 0; i < 50;i++) {
+//		Enqueue(i, q1);
+//		printf("%5d 已入队。队列是否排满 %5d\r\n",i,IsFull(q1) );
+//	}
+//
+//	while (!IsEmpty(q1)) {
+//		printf("%5d 已出队。\r\n", FrontAndDequeue(q1));
+//	}
+//
+//	return 0;
+//}
+
+
+int main(void)
+{
+	int i;
+
+
+
+	return 0;
+}
+
+
+
